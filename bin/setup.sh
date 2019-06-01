f2cfc10a1afc6b5930aa926e303d2cb6e55f55c2
clayton <me@clayton.cc>
2019-06-01 05:16:44 -0700

    Move setup.sh to bin

---
+++ bin/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+npm install
+
+mkdir -p config storage
+
+cp config.json.example config/config.json
+touch config/document
+touch config/news-post-header.md
+touch config/news-post-intro.md
