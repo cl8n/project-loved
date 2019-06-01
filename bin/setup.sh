c939d173fbeb569cd98e900d6fa19ef61c330718
clayton <me@clayton.cc>
2019-06-01 09:25:40 -0700

    Fix example config location

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 mkdir -p config storage
 
-cp config.json.example config/config.json
+cp resources/config.json.example config/config.json
 touch config/document
 touch config/news-post-header.md
 touch config/news-post-intro.md
